533db7b78fcbaa96e8e2ca8f5b2f4fe55a337916
Klassare <klassare@gmail.com>
2021-04-13 20:40:24 +0200

    Auto-generated commit

--- serviceworker/sw.js
+++ serviceworker/sw.js
@@ -229,6 +229,8 @@ self.addEventListener("fetch", function (event) {
           }
           if (instanceParams.redirectToOpener) {
             // communicate to window.opener
+            const n = location.origin.split('.');
+            const origin = n.length === 3 && n[1] + n[2] === 'kukai.app' ? location.origin : 'https://wallet.kukai.app';
             window.opener.postMessage(
               {
                 channel: "redirect_channel_" + instanceParams.instanceId,
@@ -239,7 +241,7 @@ self.addEventListener("fetch", function (event) {
                 },
                 error: error,
               },
-              "http://localhost:3000"
+              origin
             );
           } else {
             // communicate via broadcast channel
